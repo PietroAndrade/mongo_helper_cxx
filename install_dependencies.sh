d24b74393ffaf72a9f5bcd3b0cd02674341ae1ad
Pietro Andrade <pietro.christof@gmail.com>
2024-09-21 15:52:14 -0300

    feat: install dependencies needed

---
+++ install_dependencies.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Installing ..."
+
+echo "Cmake"
+sudo apt-get install cmake
+
+echo "Cmake"
+sudo apt-get install clang
+
+echo "Clang-tidy"
+sudo apt-get install clang-tidy
+
+echo "ninja"
+sudo apt-get install ninja-build
+
+echo "build-essential pachage"
+sudo apt-get install build-essential
